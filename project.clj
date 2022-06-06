e1bcd132a7020e9dbbae5e6cb064eb3610535c10
ronniedroid <ronni.n.yousif@gmail.com>
2022-06-06 10:04:30 +0300

    changed license

--- project.clj
+++ project.clj
@@ -1,17 +1,16 @@
-(defproject getnf-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license
-  {:name
-   "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0",
-   :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject getnf-clj "0.1.0"
+  :description
+  "A better way to install NerdFonts, Clojure Version"
+  :url "http://github.com/ronniedroid/getnf-clj"
+  :license {:name "The MIT License (MIT)",
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.cli "1.0.206"]
                  [cheshire "5.10.2"]
                  [clj-http "3.12.3"]
                  [clj-fuzzy "0.4.1"]
                  [clj-file-zip "0.1.0"]]
-  :main ^:skip-aot getnf-clj.core
+  :main getnf-clj.core
   :target-path "target/%s"
   :profiles
   {:uberjar
